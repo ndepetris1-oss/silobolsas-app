4995a66e676db720d17b5a4a98ba8e3b99704d6b
ndepetris1 <ndepetris1@gmail.com>
2026-03-06 12:46:36 -0300

    Migracion a PostgreSQL para Render

--- db.py
+++ db.py
@@ -1,10 +1,25 @@
 import sqlite3
 import os
+import psycopg2
+import psycopg2.extras
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_NAME = os.path.join(BASE_DIR, "silobolsas.db")
 
+
 def get_db():
+
+    database_url = os.environ.get("DATABASE_URL")
+
+    # Si estamos en Render → usar PostgreSQL
+    if database_url:
+        conn = psycopg2.connect(
+            database_url,
+            cursor_factory=psycopg2.extras.RealDictCursor
+        )
+        return conn
+
+    # Si estamos local → usar SQLite
     conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
     return conn
